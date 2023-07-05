e13cfda086c13d2164047508ae5aadff5002f6f3
Chad Kirby <chad.kirby@gmail.com>
2023-07-05 14:38:16 -0700

    redirect from / to default mixture

---
+++ src/routes/+page.server.js
@@ -0,0 +1,15 @@
+import { Mixture, Spirit, Water, Sugar } from '$lib';
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export function load() {
+    /** @type {{name: string, component: import('$lib').Component}[]} */
+    const components = [
+        { name: 'spirit', component: new Spirit(100, 40) },
+        { name: 'water', component: new Water(100) },
+        { name: 'sugar', component: new Sugar(50) }
+    ]
+    const mixture = new Mixture(components);
+
+    throw redirect(307, `/${encodeURIComponent('My Mixture')}?${mixture.serialize()}`);
+}
